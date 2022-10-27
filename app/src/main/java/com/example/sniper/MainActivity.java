c412af1a9a512c92a5180c0e6f66580c57e2075f
PetroUst <petroust2@gmail.com>
2022-10-27 17:18:25 +0300

    vertical_comp

--- app/src/main/java/com/example/sniper/MainActivity.java
+++ app/src/main/java/com/example/sniper/MainActivity.java
@@ -18,11 +18,31 @@ public class MainActivity extends AppCompatActivity {
     }
     public void calculate (View v)
     {
-        Shot s = new Shot();
-        TextView res_vertical = (TextView)findViewById(R.id.vertical_res);
-        double a =10.2;
-        res_vertical.setText(Double.toString((a)));
+        TextView res_vertical = (TextView) findViewById(R.id.vertical_res);
+        //дістаю дані з текстових полів на головному екрані
+        try {
+            EditText distance = (EditText) findViewById(R.id.edit_distance);
+            EditText wind_speed = (EditText) findViewById(R.id.edit_wind_speed);
+            EditText wind_degree = (EditText) findViewById(R.id.edit_wind_degree);
+            EditText target_height = (EditText) findViewById(R.id.edit_target_height);
+            EditText bullet_speed = (EditText) findViewById(R.id.edit_bullet_speed);
+            EditText bc = (EditText) findViewById(R.id.edit_bc);
+
+
+            //конвертую строки в дабл і передаю в поля класу shot
+            Shot.setDistance(Double.parseDouble(distance.getText().toString()));
+            Shot.setBc(Double.parseDouble(bc.getText().toString()));
+            Shot.setWind_degree(Double.parseDouble(wind_degree.getText().toString()));
+            Shot.setWind_speed(Double.parseDouble(wind_speed.getText().toString()));
+            Shot.setTarget_high(Double.parseDouble(target_height.getText().toString()));
+            Shot.setBullet_speed(Double.parseDouble(bullet_speed.getText().toString()));
+
+
+            res_vertical.setText(Double.toString((Shot.calculate_vertical_correction())));
 //        Double.toString(s.calculate_vertical_correction())
+        }catch (Exception ex){
+            res_vertical.setText("Введіть всі дані");
+        }
     }
 
 }
--- app/src/main/java/com/example/sniper/Shot.java
+++ app/src/main/java/com/example/sniper/Shot.java
@@ -1,8 +1,6 @@
 package com.example.sniper;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -11,19 +9,76 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Shot extends Activity {
-    static double distance;
-    static double target_high;
-    static double bc;//балістичний коефіцієнт
-    static double wind_speed;
-    static double bullet_speed;
-    static double wind_degree;
 
-      double calculate_vertical_correction(){
-        EditText _distance = (EditText)this.findViewById(R.id.edit_distance);
-        distance=Double.parseDouble(_distance.getText().toString());
-        double res=distance;
-        return res;
+public class Shot {
+
+    private static double distance;
+    private static double target_high= 0.00001;
+    private static double bc;//балістичний коефіцієнт
+    private static double wind_speed = 0.000001;
+    private static double bullet_speed;
+    private static double wind_degree;
+
+    public static void setBullet_speed(double bullet_speed) {
+        Shot.bullet_speed = bullet_speed;
+    }
+
+    public static void setTarget_high(double target_high) {
+        Shot.target_high = target_high;
+        if(target_high==0)
+        {
+            Shot.target_high=0.01;
+        }
+    }
+
+    public static void setDistance(double distance) {
+        Shot.distance = distance;
+    }
+
+    public static void setBc(double bc) {
+        Shot.bc = bc;
+    }
+
+    public static void setWind_degree(double wind_degree) {
+        Shot.wind_degree = wind_degree;
+    }
+
+    public static void setWind_speed(double wind_speed) {
+        Shot.wind_speed = wind_speed;
+    }
+
+    public static double calculate_vertical_correction(){
+        //EditText _distance = (EditText)this.findViewById(R.id.edit_distance);
+        //distance=Double.parseDouble(.getText().toString());
+        //EditText distance = (EditText)findViewById(R.id.edit_distance);
+        double alpha1 = 0, alpha2 = 80 * Math.PI / 180, mid=0, hCur = 0;
+        double vX, vY,T,l,t=0.00001,F,aX,aY;
+        double cur_distance=0, bullet_area =4.8e-5,
+        air_density = 1.2,weight=0.010886;
+        while(Math.abs(target_high-hCur)>0.0001){
+            mid = (alpha1 + alpha2) / 2;
+            vX = bullet_speed * Math.cos(mid);
+            vY = bullet_speed * Math.sin(mid);
+            hCur=0;
+            T=0;
+            l=0;
+            while(l<distance){
+                T+=t;
+                F=bc*air_density*bullet_area*vX*vX/2;
+                aX=F/weight;
+                vX-=aX*t;
+
+                aY = 9.80665;
+                vY-= aY*t;
+                hCur += vY * t;
+                l += vX * t;
+            }
+            if (hCur > target_high) alpha2 = mid;
+            else alpha1 = mid;
+        }
+        mid= mid* 180 / Math.PI;
+        String.format("%.3f",mid);
+        return mid;
     }
 
 
