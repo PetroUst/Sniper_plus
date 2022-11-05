86de38f2ad8d3e9dc70f489f94fb784c5cc726b9
Alaska2222 <100542322+Alaska2222@users.noreply.github.com>
2022-11-05 13:15:28 +0200

    rework vertical correction

--- app/src/main/java/com/example/sniper/MainActivity.java
+++ app/src/main/java/com/example/sniper/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public void calculate (View v)
     {
         TextView res_vertical = (TextView) findViewById(R.id.vertical_res);
-        //дістаю дані з текстових полів на головному екрані gbdgfdtrf
+        //дістаю дані з текстових полів на головному екрані gbgft
         try {
             EditText distance = (EditText) findViewById(R.id.edit_distance);
             EditText wind_speed = (EditText) findViewById(R.id.edit_wind_speed);
